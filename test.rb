5676729bdcdcc8a054fb44d1e5302f8e7c234210
Dustin Trauernicht <dustintrauernicht@gmail.com>
2017-08-04 12:11:02 -0500

    after summeet help

--- test.rb
+++ test.rb
@@ -1,26 +1,56 @@
 require "CSV"
 require "pry"
 
-    data = {}
-    soniatable = {}
+data = {}
 
 CSV.foreach("accounts.txt", {headers: true, return_headers: false}) do |row|
         inflow = row["Inflow"].gsub(/[^\d\.]/, '').to_f
         outflow = row["Outflow"].gsub(/[^\d\.]/, '').to_f
         a = row ["Account"].chomp
-        b = row ["Category"]
+        category = row ["Category"]
+
+        amount = (inflow - outflow)
 
         if data[a] == nil
-            data[a] = (inflow - outflow)
-        else
-            data[a] = data[a] + (inflow - outflow)
+            data[a] = {}
+        # else
+            # data[a] = data[a] + (inflow - outflow)
         end
 
-        if soniatable[b] == nil && data[a]
-            soniatable[b] = (inflow - outflow)
-        else
-            soniatable[b] = soniatable[b] + (inflow - outflow)
+        if data[a][category] == nil
+            data[a][category] = []
         end
+
+        if data[a][category]
+
+        # if soniatable[b] == nil
+        #    soniatable[b] = (inflow - outflow)
+        # else
+        #     soniatable[b] = soniatable[b] + (inflow - outflow)
+        # end
 end
 
-    puts soniatable
+puts data
+
+
+# data = 
+#   priya
+#     allowance: [n, n, n]
+#     dinig: [n, n, n]
+#     gym: [n, n, n]
+#   sonia
+#     allowance: [n,n,n]
+#     fuel: [n, n]
+
+# data =  {
+#     "prya" => {
+#         "allowance" => [332, 654],
+#         "dining" => [332, 654],
+#         "gym" => [332, 654]
+#     },
+
+#     "sonia" => {
+#         "allowance" => [312, 312],
+#         "fueld" => [3243423, 423423]
+#     }
+# }
