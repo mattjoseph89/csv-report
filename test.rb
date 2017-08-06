7ef9def223a6f948b6cbc3aa4d793f196664f853
Dustin Trauernicht <dustintrauernicht@gmail.com>
2017-08-06 17:47:48 -0500

    html formatting, averages, and total balance added

--- test.rb
+++ test.rb
@@ -2,6 +2,7 @@ require "CSV"
 require "pry"
 
 data = {}
+balance = {}
 
 CSV.foreach("accounts.txt", {headers: true, return_headers: false}) do |row|
         inflow = row["Inflow"].gsub(/[^\d\.]/, '').to_f
@@ -10,17 +11,24 @@ CSV.foreach("accounts.txt", {headers: true, return_headers: false}) do |row|
         category = row ["Category"].chomp
         amount = (inflow - outflow)
 
-
-        if data[accounts] == nil
-            data[accounts] = {}
-
+        if balance[accounts] == nil
+            balance[accounts] = [amount]
+        else
+            balance[accounts] += [amount]
         end
 
-        if data[accounts][category] == nil
+
+        if 
+            data[accounts] == nil
+            data[accounts] = {}
+        end
+
+
+        if 
+            data[accounts][category] == nil
             data[accounts][category] = [amount]
-
-        else data[accounts][category]+= [amount]
-
+        else
+            data[accounts][category] += [amount]
         end
 
         # if soniatable[b] == nil
@@ -30,16 +38,46 @@ CSV.foreach("accounts.txt", {headers: true, return_headers: false}) do |row|
         # end
 end
 
-data.each  do |account, categories|
-  puts "============================"
-  puts "#{account}"
-  puts "============================"
-  puts "Category    | Total Spent | Average Transaction   "
-  puts "----------------------------"
-  categories.each do |category,transactions|
-    puts "#{category}  #{transactions.sum.round(2)}"
+input_array = ARGV
+
+if input_array.include? "html"
+
+data.each do |account, categories|
+  puts "<h1>#{account}</h1>"
+  puts "<p>Total Balance: $#{balance[account].sum}</p>"
+  puts "<hr>"
+  puts "<table>"
+  puts "    <tr>"
+  puts "        <th>Category</th>"
+  puts "        <th>Total Spent</th>"
+  puts "      <th>Avg. Transaction</th>"
+  puts "    </tr>"
+categories.each do |category, transactions|
+  puts "\n"
+  puts "    <tr>"
+  puts "        <td>#{category}</td>"
+  puts "        <td>$#{transactions.sum.round(2)}</td>"
+  puts "        <td>$#{(transactions.sum.round(2)/transactions.count).round(2)}</td>"
+  puts "    <tr>"
   end
+  end
+
+else
+
+data.each  do |account, categories|
+  puts "======================================================================"
+  puts "Account: #{account}... Balance: $#{balance[account].sum}"
+  puts "======================================================================"
+  printf("%-29s%-14s%-27s\n", "Category", "Total Spent", "Average Transaction")
+  puts "---------------------------- |------------ |--------------------------"
+categories.each do |category,transactions|
+  printf("%-29s%-14s%-27s\n", "#{category}", "| $#{transactions.sum.round(2)}", "| $#{(transactions.sum.round(2)/transactions.count).round(2)}")
+  #puts  "#{category} $#{transactions.sum.round(2)} $#{(transactions.sum.round(2)/transactions.count).round(2)}"
+  end
+  end
+
 end
+
 
 
 # puts data
